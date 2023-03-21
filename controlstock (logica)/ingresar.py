698ebedaa37ddfc4951d46f4a32183e4ae04972c
Toulousegg <90469033+Toulousegg@users.noreply.github.com>
2023-03-21 20:13:00 -0300

    Logica + base Django
    Anadí la base de la aplicación usando el framework Django (quiero que esta aplicación este creada en ese framework pero aún no he codeado nada ahí) y agregué la lógica con python puro hecho a medias porque mi editor de texto se fue a la mrd, pero la parte más importante del programa esta hecha

---
+++ controlstock (logica)/ingresar.py
@@ -0,0 +1,36 @@
+from turtle import *
+
+dimensiones = input('Escribe las dimensiones: ')
+separador = 'x'
+dim_sep = dimensiones.split(separador)
+veio = input('Escribe la direccion del veio: ')
+veioint = int(veio)
+
+def grafico():
+    # cuantas veces va a girar 
+    for i in range (2): 
+        forward(60)
+        right(90)
+        forward(100)
+        right(90)
+
+def grafico2():
+    # cuantas veces va a girar
+    for i in range (2): 
+        forward(60)
+        right(90)
+        forward(100)
+        right(90)
+
+if veio == dim_sep[0]:
+    print('El veio apunta hacia ' + dim_sep[0])
+    grafico()
+
+if veio == dim_sep[1]:
+    print('El veio apunta hacia ' + dim_sep[1])
+    grafico2()
+
+if veio == 0:
+    print('Pieza sin veio')
+
+input('presiona enter para salir: ')
